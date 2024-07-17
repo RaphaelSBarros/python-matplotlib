1178703a0dcb5235d77505d64157c3c2e20ee3c0
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-17 10:41:33 -0300

    Default x-points

--- intro_pyplot.py
+++ intro_pyplot.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([1, 2, 6, 8])
-ypoints = np.array([3, 8, 1, 10])
+ypoints = np.array([3, 8, 1, 10, 5, 7])
 
-plt.plot(xpoints, ypoints)
+plt.plot(ypoints)
 plt.show()
