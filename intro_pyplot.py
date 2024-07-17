0f53c74bd1752dd0125a6f37844b7d3fdac604a2
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-17 14:05:34 -0300

    Title position

--- intro_pyplot.py
+++ intro_pyplot.py
@@ -7,7 +7,7 @@ y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 font1 = {'family':'serif','color':'blue','size':20}
 font2 = {'family':'serif','color':'darkred','size':15}
 
-plt.title("Sports Watch Data", fontdict = font1)
+plt.title("Sports Watch Data", fontdict = font1, loc='left')
 plt.xlabel("Average Pulse", fontdict = font2)
 plt.ylabel("Calorie Burnage", fontdict = font2)
 
