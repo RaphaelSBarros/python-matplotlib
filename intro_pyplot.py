259640ad13f13acf5023ab6d555b059481bf006b
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-17 13:56:27 -0300

    Plotting two lines by specifiyng the x- and y-point values for both lines

--- intro_pyplot.py
+++ intro_pyplot.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-ypoints = np.array([3, 8, 1, 10])
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
 
-plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.plot(x1, y1, x2, y2)
 plt.show()
